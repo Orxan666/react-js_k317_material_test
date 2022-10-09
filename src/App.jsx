db293a1454c57d16503fa324365fb63b9790893f
orxan666 <orxanallahyarov03@gmail.com>
2022-10-09 12:20:23 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import { useEffect, useState } from "react";
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
 
+const Item = styled(Paper)(({ theme }) => ({
+  // backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  // color: theme.palette.text.secondary,
+}));
 function App() {
   const [users, setUsers] = useState([])
   const [name, setName] = useState("Ayxan")
@@ -18,19 +30,23 @@ function App() {
   return (
     <div className="App">
       {/* <button onClick={() => loadData()}>Load data</button> */}
+      <Grid container spacing={3}>
       {
         users.map(user => (
-          <div key={user.id}>
-            <h1>{user.name}</h1>
-            <p>{user.email}</p>
-            
-            {
-              <p>{user.address.street}</p>
-            }
-            <hr />
-          </div>
+          <Grid xs={4} key={user.id}>
+            <Item>
+                <h1>{user.name}</h1>
+                <p>{user.email}</p>
+                
+                {
+                  <p>{user.address.street}</p>
+                }
+            </Item>
+          </Grid>
+          
         ))
       }
+      </Grid>
     </div>
   );
 }
